7598bd44f2ef38dfea22a2714fe977c0a3bfa8dd
mehonje <168048262+mehonje@users.noreply.github.com>
2025-07-01 20:36:32 -0500

    Update sprite.py
    Changed the stamp function to support transparency
      self.stamp now has an argument, "transparency"
      "transparency" controls the transparency of the stamped costume
      0 is opaque, 100 is transparent
    Changed the convertCoords function to support calculations without rects
      When drawing lines, which have no rect, you can pass "" as the rect argument to calculate the position of the line's endpoints

--- scratch/sprite.py
+++ scratch/sprite.py
@@ -3,15 +3,22 @@ import pygame
 class sprite:
     def __init__(self, gameEngine, costume):
         self.x, self.y = 0, 0
-        self.dir = 180
+        self.dir = 0
         self.screen = gameEngine.screen
         self.screenBounds = gameEngine.screen.get_rect()
-        self.costumePath = pygame.image.load("C:/users/jack/onedrive/desktop/pywork/lemonoids/costumes/" + costume)
+        self.setCostume(costume)
         self.rect = self.costumePath.get_rect()
         self.gameEngine = gameEngine
 
-    def stamp(self):
+    def setCostume(self, costume):
+        if costume != "":
+            self.costumePath = pygame.image.load("costumes/" + costume)
+        else:
+            self.costumePath = pygame.image.load("costumes/costume1.png")
+
+    def stamp(self, transparency):
         self.costume = pygame.transform.rotate(self.costumePath, self.dir)
+        self.costume.set_alpha(255 * (1 - transparency / 100))
         self.rect = self.costume.get_rect()
         self.rect.x, self.rect.y = convertCoords(self.x, self.y, self.rect)
         self.screen.blit(self.costume, self.rect)
@@ -19,6 +26,10 @@ class sprite:
 def convertCoords(x, y, rect, screenWidth = 480, screenHeight = 360):
     #(0, 0) = (240, 180) = (screenWidth / 2, screenHeight / 2)
 
-    x = ((screenWidth / 2) + x) - (rect.width / 2)
-    y = ((screenHeight / 2) - y) - (rect.height / 2)
+    if rect != "":
+        x = ((screenWidth / 2) + x) - (rect.width / 2)
+        y = ((screenHeight / 2) - y) - (rect.height / 2)
+    else:
+        x = (screenWidth / 2) + x
+        y = (screenHeight / 2) - y
     return int(x), int(y)
