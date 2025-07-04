be00798b20db5ddadc795aca6a4d213110b66348
mehonje <168048262+mehonje@users.noreply.github.com>
2025-07-04 15:55:41 -0500

    Update sprite.py

--- scratch/sprite.py
+++ scratch/sprite.py
@@ -1,9 +1,10 @@
 import pygame
 
-class sprite:
+class Sprite:
     def __init__(self, gameEngine, costume):
         self.x, self.y = 0, 0
         self.dir = 0
+        self.size = 100
         self.screen = gameEngine.screen
         self.screenBounds = gameEngine.screen.get_rect()
         self.setCostume(costume)
@@ -12,24 +13,24 @@ class sprite:
 
     def setCostume(self, costume):
         if costume != "":
-            self.costumePath = pygame.image.load("costumes/" + costume)
+            self.costumePath = pygame.image.load("costumes/" + costume).convert_alpha()
         else:
-            self.costumePath = pygame.image.load("costumes/costume1.png")
+            self.costumePath = pygame.image.load("costumes/costume1.png").convert_alpha()
 
-    def stamp(self, transparency):
-        self.costume = pygame.transform.rotate(self.costumePath, self.dir)
-        self.costume.set_alpha(255 * (1 - transparency / 100))
+    def stamp(self, transparency, size):
+        self.costume = pygame.transform.rotate(self.costumePath, -self.dir)
         self.rect = self.costume.get_rect()
+        self.costume = pygame.transform.scale(self.costume, (self.rect.width * (size * 0.01), self.rect.height * (size * 0.01)))
         self.rect.x, self.rect.y = convertCoords(self.x, self.y, self.rect)
+        self.costume.set_alpha(255 * (1 - transparency / 100))
         self.screen.blit(self.costume, self.rect)
 
 def convertCoords(x, y, rect, screenWidth = 480, screenHeight = 360):
-    #(0, 0) = (240, 180) = (screenWidth / 2, screenHeight / 2)
-
-    if rect != "":
-        x = ((screenWidth / 2) + x) - (rect.width / 2)
-        y = ((screenHeight / 2) - y) - (rect.height / 2)
-    else:
-        x = (screenWidth / 2) + x
-        y = (screenHeight / 2) - y
+    match rect:
+        case "":
+            x = (screenWidth / 2) + x
+            y = (screenHeight / 2) - y
+        case _:
+            x = ((screenWidth / 2) + x) - (rect.width / 2)
+            y = ((screenHeight / 2) - y) - (rect.height / 2)
     return int(x), int(y)
