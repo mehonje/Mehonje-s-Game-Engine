67e7828275fdda7c536a8bf7b8e879366e119f7f
mehonje <168048262+mehonje@users.noreply.github.com>
2025-07-04 15:57:09 -0500

    Update scratch.py

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,17 +1,20 @@
 import sys
 import pygame
-from sprite1 import sprite1
+from sprite1 import Sprite1
 
 class gameEngine:
     def __init__(self, gameName):
         pygame.init()
+        print(f"Thanks for playing {gameName}!")
+        print("You can find me at https://scratch.mit.edu/users/mehonje/ and https://github.com/mehonje")
+        print("This game was made using Mehonje's Game Engine - https://github.com/mehonje/Mehonje-s-Game-Engine/tree/main")
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((480, 360))
         pygame.display.set_caption(gameName)
         self.setup()
 
     def setup(self):
-        self.sprite1 = sprite1(self, "costume1.png")
+        self.sprite1 = Sprite1(self, "costume1.png")
 
     def runGame(self):
         self.running = True
